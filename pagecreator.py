8ac40462cf607630214fe94103a5e05cf345e91a
chipperdoodles <myknobel@gmail.com>
2015-03-25 17:13:04 -0700

    added pagecreator script

---
+++ pagecreator.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+class Page(object):
+    def __init__(self, *args, **kwargs):
+
+        self.input =
+
+    def file_name():
+        basename = 'HOWTO'
+        extension = '.md'
+        number = 
+        fullname = basename+'_'+number+extension
+        return fullname =filename
+
+    def add_number(self, ):
+        self.basename
+
+    def contents():
+        title = ""
+        published = 2016-02-15
+        modified =
+        page_type = book
+        book = { 'title': "side comic", 'chapter': 1, 'page_number': 1, 'image': "" }
+        menu = False
+        version: 0.1
+
+    def write_page():
+        with open(self.filename,"ab") a f:
+            f.write()
