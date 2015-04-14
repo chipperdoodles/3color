ac3e7f0659c048ab7e02b713fb47384c20c13f2c
chipperdoodles <myknobel@gmail.com>
2015-04-14 03:06:08 -0700

    added open in file browser tool, testing page creation tools

---
+++ threecolor/tools/misc.py
@@ -0,0 +1,16 @@
+import sys
+import subprocess
+
+from ..configs import sysinfo
+from ..application import instfolder
+
+def open_browser():
+    if sysinfo.system=='Windows':
+        startfile(instfolder['open'])
+    elif sysinfo.system=='Darwin':
+        subprocess.call(["open", instfolder])
+    else:
+        try:
+            subprocess.call(['xdg-open', instfolder])
+        except OSError:
+            print('Operating system doesn\'t have lazy file browser opener)
