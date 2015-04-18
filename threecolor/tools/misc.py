41e568a591a357026befc180938acb88bafe84bb
ievans3024 <ievans3024@gmail.com>
2015-04-17 18:05:05 -0700

    * fixed startfile reference so it doesn't trigger pydev code linter for non-windows environments

--- threecolor/tools/misc.py
+++ threecolor/tools/misc.py
@@ -1,16 +1,17 @@
+import os
 import subprocess
 
 from ..configs import sysinfo
 from ..application import instfolder
-from os import startfile  # FIXME: not python3 friendly
+
 
 def open_browser():
-    if sysinfo.system=='Windows':
-        startfile(instfolder['open'])
-    elif sysinfo.system=='Darwin':
+    if sysinfo.system == 'Windows':
+        os.startfile(instfolder['open'])
+    elif sysinfo.system == 'Darwin':
         subprocess.call(["open", instfolder])
     else:
         try:
             subprocess.call(['xdg-open', instfolder])
         except OSError:
-            print('Operating system doesn\'t have lazy file browser opener')
+            print("Operating system doesn't have lazy file browser opener")
